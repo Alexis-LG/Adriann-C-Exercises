70b7d8bf0fe9a7e6a959f3a4ac3773f20e07693e
Alexis <20258824+CosmicSausage@users.noreply.github.com>
2017-11-21 18:09:18 -0400

    Add files via upload

---
+++ ListSum.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElementaryCSharpExercises
+{
+    class ListSum
+    {
+        public void Sum()
+        {
+            //7 - Write three functions that compute the sum of the numbers in a list: using a for-loop, a while-loop and recursion. 
+            //(Subject to availability of these constructs in your language of choice.)
+            var numberList = new List<int> { 10, 20, 30 };
+            var numberListTotal = 0;
+            var i = 0;
+            for (i = 0; i < numberList.Count; i++)
+                numberListTotal += numberList[i];
+
+            Console.WriteLine(numberListTotal);
+
+            while (i < numberList.Count)
+            {
+                numberListTotal += numberList[i];
+                i++;
+            }
+            Console.WriteLine(numberListTotal);
+            // Recursion part needs to be added
+        }
+    }
+}
---
+++ PalindromeTester.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElementaryCSharpExercises
+{
+    class PalindromeTester
+    {
+        public static string ReverseString(string stringComparer)
+        {
+            char[] stringArray = stringComparer.ToCharArray();
+            Array.Reverse(stringArray);
+            return new string(stringArray);
+        }
+
+        public void TestForPalindrome(string compareValue)
+        {
+            ////6 - Write a function that tests whether a string is a palindrome.
+            var stringToCompare = compareValue;
+            if (stringToCompare == ReverseString(stringToCompare))
+                Console.WriteLine("This string is a Palindrome");
+            else if (stringToCompare != ReverseString(stringToCompare))
+                Console.WriteLine("This string is not a Palindrome");
+            else
+                Console.WriteLine("Damn son, where did you find this?");
+        }
+    }
+}
